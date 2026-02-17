9af67493c78094b7127a7086261277f4ef8b06b7
stritar <3016106+stritar@users.noreply.github.com>
2026-02-17 19:30:02 +0100

    adsas
    dada

--- area51/src/App.jsx
+++ area51/src/App.jsx
@@ -1,24 +1,8 @@
-import { Card } from './components/Card/Card';
 import styles from './App.module.css';
 
 export default function App() {
   return (
     <div className={styles.container}>
-      <Card 
-        className={styles.cardTall} 
-        icon="placeholder" 
-        heading="Card one" 
-      />
-      <Card 
-        className={styles.card} 
-        icon="image" 
-        heading="Card two" 
-      />
-      <Card 
-        className={styles.card} 
-        icon="text" 
-        heading="Card three" 
-      />
     </div>
   );
 }
